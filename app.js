d7c1e4e79e989b9f8357b43c9e38101d079c0c23
Hayden <chfrank9011@gmail.com>
2019-03-28 12:05:42 -0500

    final commit

--- app.js
+++ app.js
@@ -1,25 +1,72 @@
 let btn = document.createElement('button');
 let btnText = document.createTextNode('Add Square');
+let mainContainer = document.createElement('article');
 
 document.body.appendChild(btn);
 btn.appendChild(btnText);
+document.body.appendChild(mainContainer);
+
 
 
 btn.className = 'addDiv';
 
-btn.addEventListener('click', function(){
-   let squareDiv = document.createElement('div');
-   document.body.appendChild(squareDiv);
-    
-   squareDiv.style.backgroundColor = 'black';
-    squareDiv.style.width = '75px';
-    squareDiv.style.height = '75px';
-    squareDiv.style.cssFloat = 'left';
-    squareDiv.className = 'squares';
-    squareDiv.setAttribute('id', id);
+let id = '0';
+
+let rainbow = ['DeepPink', 'FireBrick', 'GreenYellow', 'Lavender', 'Wheat', 'SpringGreen', 'Sienna', 'PowderBlue'];
+
+function getRandomColor() {
+    let randomColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+    return randomColor;
+}
+
+btn.addEventListener('click', function () {
+    let square = document.createElement('div');
+    mainContainer.appendChild(square);
+
+    square.style.backgroundColor = 'black';
+    square.style.width = '75px';
+    square.style.height = '75px';
+    square.style.cssFloat = 'left';
+    square.className = 'squares';
+    square.setAttribute('id', id);
     id++;
 
+    square.addEventListener('mouseover', function () {
+        square.style.color = 'white';
+        square.textContent = square.id;
+    })
+
+    square.addEventListener('mouseout', function () {
+        square.textContent = '';
+    }
+    );
+
+    square.addEventListener('click', function () {
+        square.style.backgroundColor = getRandomColor();
+    });
+
+    square.addEventListener('dblclick', function () {
+        if (square.id % 2 === 0) {
+            if (square.nextSibling === null) {
+                alert('No square to delete!');
+            } else {
+                square.nextSibling.remove();
+            }
+        } else {
+                if (square.previousSibling === null) {
+                alert('No square to delete!');
+            } else {
+                square.previousSibling.remove();
+            }
+        }
+
+    })
+
 });
+
+
+
+
 
 
 
